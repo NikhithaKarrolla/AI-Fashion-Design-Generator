53f9892001137ea20df7fefddf4676c4d2b5ae08
NikhithaKarrolla <knikhitha10@gmail.com>
2026-02-26 18:02:50 +0530

    Changed every thing

--- design_generator.py
+++ design_generator.py
@@ -1,18 +1,22 @@
-import streamlit as st
-from diffusers import AutoPipelineForText2Image
-import torch
-
-@st.cache_resource
-def load_model():
-    pipe = AutoPipelineForText2Image.from_pretrained(
-        "stabilityai/sd-turbo",
-        torch_dtype=torch.float32
-    )
-    pipe = pipe.to("cpu")
-    return pipe
+import numpy as np
+from PIL import Image, ImageDraw
+import os
 
 def generate_design(prompt):
-    pipe = load_model()
-    image = pipe(prompt).images[0]
-    image.save("generated/generated_design.png")
-    return "generated/generated_design.png"
+
+    # create generated folder if not exists
+    if not os.path.exists("generated"):
+        os.makedirs("generated")
+
+    # simulate AI generated feature vector (2048 same as ResNet50)
+    generated_feature = np.random.rand(2048)
+
+    # create dummy generated design image
+    img = Image.new('RGB', (256,256), color=(200,200,200))
+    draw = ImageDraw.Draw(img)
+    draw.text((40,120), "AI Design", fill=(0,0,0))
+
+    img_path = "generated/generated_design.png"
+    img.save(img_path)
+
+    return generated_feature, img_path
