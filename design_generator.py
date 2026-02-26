851ad85d138010ecea809eeef3e902666668bab4
NikhithaKarrolla <knikhitha10@gmail.com>
2026-02-26 08:51:21 +0530

    Replaced heavy SD model with tiny CPU model

--- design_generator.py
+++ design_generator.py
@@ -1,18 +1,20 @@
-import streamlit as st
 from diffusers import AutoPipelineForText2Image
 import torch
 
-@st.cache_resource
-def load_model():
-    pipe = AutoPipelineForText2Image.from_pretrained(
-        "stabilityai/sd-turbo",
-        torch_dtype=torch.float32
-    )
-    pipe = pipe.to("cpu")
-    return pipe
+pipe = AutoPipelineForText2Image.from_pretrained(
+    "hf-internal-testing/tiny-stable-diffusion-pipe",
+    torch_dtype=torch.float32
+)
+
+pipe = pipe.to("cpu")
 
 def generate_design(prompt):
-    pipe = load_model()
-    image = pipe(prompt).images[0]
-    image.save("generated/generated_design.png")
-    return "generated/generated_design.png"
+
+    image = pipe(
+        prompt=prompt,
+        num_inference_steps=10
+    ).images[0]
+
+    image.save("generated/design.png")
+
+    return "generated/design.png"
